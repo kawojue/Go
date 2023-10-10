0c5adc59ec3546e870f4e4202c2bd0bbcdf44b73
Raheem Kawojue <kawojue08@gmail.com>
2023-10-10 11:13:51 +0100

    .

---
+++ 3/index.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	happy()
+	fmt.Print(dance("Kawojue"))
+}
+
+func dance(name string) string {
+	return fmt.Sprintf("%s dances", name)
+}
+
+func happy() {
+	fmt.Println("Once again, I love Golang!")
+}
