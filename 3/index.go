a54ad8439eafad85851f6f688226fd17377a5b12
Raheem Kawojue <kawojue08@gmail.com>
2023-10-10 14:49:32 +0100

    concurrency & parallelism

--- 3/index.go
+++ 3/index.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	dance := dance("Kawojue")
 
-	happy()
-	fmt.Print(dance)
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		happy()
+	}()
+
+	fmt.Println(dance)
+
+	wg.Wait()
 }
 
 func dance(name string) string {
