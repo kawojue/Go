4ed9a4df63c083ba56db01ef10ebaf081241c127
Raheem Kawojue <kawojue08@gmail.com>
2023-10-13 22:16:38 +0100

    conditions

---
+++ 8/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for true {
+		fmt.Print("How old are you? ")
+		scanner.Scan()
+
+		age, _ := strconv.ParseInt(scanner.Text(), 10, 32)
+
+		if age < 18 {
+			fmt.Println("You're underage.")
+		} else if age >= 32 {
+			fmt.Println("You should get married.")
+		} else {
+			fmt.Println("Have fun!")
+		}
+
+		fmt.Println()
+	}
+}
