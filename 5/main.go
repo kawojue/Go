8bed31a9470f795c050697d451d4eaef216d97b7
Raheem Kawojue <kawojue08@gmail.com>
2023-10-13 12:41:35 +0100

    calc age

--- 5/main.go
+++ 5/main.go
@@ -4,18 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Input your text: ")
 	scanner.Scan()
-
 	text := scanner.Text()
 
-	print(text)
+	println(text)
+
+	fmt.Print("Year you were born: ")
+	scanner.Scan()
+	year, _ := strconv.ParseInt(scanner.Text(), 10, 32)
+	fmt.Printf("You are %d years old.\n", calcAge(year))
 }
 
-func print(text string) {
-	fmt.Printf("%s", text)
+func println(text string) {
+	fmt.Printf("%s\n", text)
+}
+
+func calcAge(year int64) int64 {
+	return 2023 - year
 }
