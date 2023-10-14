06bf1aa22667130b67ca8e2d6b4bfa585f5db5a7
Raheem Kawojue <kawojue08@gmail.com>
2023-10-15 00:57:56 +0100

    funcs

--- 9/main.go
+++ 9/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 )
 
 func sayGoodbye(name string) string {
 	return fmt.Sprintf("Goodbye, %s.", name)
 }
 
-func callback(names []string, cb func(name string) string) {
+func cycleNames(names []string, cb func(name string) string) {
 	for _, value := range names {
 		fmt.Println(cb(value))
 	}
 }
 
+func circleArea(radius float64) float64 {
+	area := math.Pi * math.Pow(radius, 2)
+	formatted, _ := strconv.ParseFloat(
+		fmt.Sprintf("%.3f", area), 64,
+	)
+	return formatted
+}
+
 func main() {
-	callback([]string{"Raheem", "Muyiwa"}, sayGoodbye)
+	cycleNames([]string{"Raheem", "Muyiwa"}, sayGoodbye)
+
+	fmt.Println(circleArea(12))
 }
