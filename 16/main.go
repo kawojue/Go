bc8d4ed4ba582cd95e91efd32cfdd358ed09dfa0
Raheem Kawojue <kawojue08@gmail.com>
2023-10-23 15:45:55 +0100

    receiver func

--- 16/main.go
+++ 16/main.go
@@ -1,10 +1,81 @@
 package main
 
-type Person struct {
+import (
+	"fmt"
+	"strings"
+)
+
+type Bill struct {
+	name  string
+	items map[string]float64
+	tip   *int64
 }
 
-func newPerson() {}
+func newBill(name string, items map[string]float64, tip *int64) Bill {
+	return Bill{
+		items: items,
+		name:  name,
+		tip:   tip,
+	}
+}
+
+func toTitle(s string) string {
+	splitted := strings.Split(s, "")
+	formatted := strings.ToUpper(splitted[0]) + strings.Join(splitted[1:], "")
+
+	return formatted
+}
 
 func main() {
+	bill1 := newBill("Raheem", map[string]float64{"egg": 5.99, "bread": 3.69}, nil)
+	fmt.Printf("%s\n%v\n%v\n", bill1.name, bill1.items, bill1.tip)
 
+	fmt.Println()
+
+	tipValue := int64(1)
+	bill2 := newBill("Kawojue", map[string]float64{"yam": 7.99}, &tipValue)
+	fmt.Printf("Name: %s\nItems: %v\nTip: %v\n", bill2.name, bill2.items, *bill2.tip)
+
+	editBill1 := bill1.editName("muyiwa")
+	fmt.Println(editBill1.name)
+
+	fmt.Println()
+
+	fmt.Println(editBill1.formatName())
+
+	fmt.Println()
+
+	bill1.billing()
+}
+
+func (bill Bill) editName(name string) Bill {
+	b := Bill{
+		name:  name,
+		tip:   bill.tip,
+		items: bill.items,
+	}
+
+	return b
+}
+
+func (bill Bill) formatName() Bill {
+	b := Bill{
+		name:  toTitle(bill.name),
+		items: bill.items,
+		tip:   bill.tip,
+	}
+	return b
+}
+
+func (bill Bill) billing() {
+	var total float64
+	breakdowns := "Bill Breakdown:\n"
+
+	for item, price := range bill.items {
+		breakdowns += fmt.Sprintf("\t%s: $%g\n", toTitle(item), price)
+		total += price
+	}
+
+	fmt.Println(breakdowns)
+	fmt.Printf("Total: $%v", total)
 }
