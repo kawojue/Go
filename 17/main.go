86746228819c1159d6f7bf837f8ab8d3134ad3a6
Raheem Kawojue <kawojue08@gmail.com>
2023-10-25 14:52:25 +0100

    struct methods

--- 17/main.go
+++ 17/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+type Point struct {
+	x int64
+	y int64
+}
+
+type Circle struct {
+	radius float64
+	*Point
+}
+
+func main() {
+	c1 := Circle{3.4, &Point{y: 2, x: 3}}
+	fmt.Println(c1.x)
 }
