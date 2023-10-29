b9d2f15a057dde06bda5570e8422dca376e06efc
Raheem Kawojue <kawojue08@gmail.com>
2023-10-30 00:02:19 +0100

    input

--- 19/main.go
+++ 19/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	text, _ := input("What? ")
+	fmt.Println(text)
+}
+
+func input(question string) (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	if question != "" {
+		fmt.Print(question)
+	}
+
+	text, err := reader.ReadString('\n')
+
+	return strings.TrimSpace(text), err
 }
