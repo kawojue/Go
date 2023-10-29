ba89c2a3d7d4b2ce1dc9e11c1415c1289248e2bf
Raheem Kawojue <kawojue08@gmail.com>
2023-10-30 00:42:49 +0100

    input

--- 19/main.go
+++ 19/main.go
@@ -4,12 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	text, _ := getInput("What? ", bufio.NewReader(os.Stdin))
 	fmt.Println(text)
+
+	promptOptions()
+}
+
+func promptOptions() {
+	reader := bufio.NewReader(os.Stdin)
+
+	option, _ := getInput("How old are you?", reader)
+	age, _ := strconv.ParseInt(option, 10, 64)
+
+	switch {
+	case age >= 18:
+		fmt.Println("You're Adult.")
+	case age < 18:
+		fmt.Println("You're underage.")
+	default:
+		fmt.Println("Invalid Age Provided.")
+	}
 }
 
 func getInput(prompt string, r *bufio.Reader) (string, error) {
