36870c85688d3a3cdcfad088e48818327a9985c7
Raheem Kawojue <kawojue08@gmail.com>
2023-11-25 23:17:54 +0100

    rune

--- 20/main.go
+++ 20/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	chars := "abcde"
+
+	ascii := map[string]rune{}
+
+	for _, char := range chars {
+		ascii[string(char)] = rune(char)
+	}
+
+	fmt.Println(ascii)
 }
