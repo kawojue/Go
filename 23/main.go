ee8669cd98fa4eb5410f0547238af32b8b419623
Raheem Kawojue <kawojue08@gmail.com>
2023-12-03 01:25:53 +0100

    working with files

--- 23/main.go
+++ 23/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -12,9 +13,19 @@ func main() {
 		fmt.Println(err)
 	}
 
-	_, err = os.Stat(fmt.Sprintf("%s\\%s", cwd, "data.txt"))
+	file := fmt.Sprintf("%s\\%s", cwd, "data.txt")
+	_, err = os.Stat(file)
 	if errors.Is(err, os.ErrNotExist) {
 		fmt.Println("File does not exist.")
-	}
+	} else {
+		f, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
 
+		if _, err := f.WriteString("Hello, World!\n"); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
