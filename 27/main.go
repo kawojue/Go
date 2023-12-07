ff63e0e0570fa83c7a717ee47be09b93f87d84ee
Raheem Kawojue <kawojue08@gmail.com>
2023-12-07 11:45:28 +0100

    working with generic

--- 27/main.go
+++ 27/main.go
@@ -8,10 +8,30 @@ type MyConstraint interface {
 	int | float64
 }
 
+type Customer struct {
+	name string
+	city string
+	bal  float64
+}
+
 func getSumGen[T MyConstraint](x T, y T) T {
 	return x + y
 }
 
+func editCity(c *Customer, city string) {
+	c.city = city
+}
+
 func main() {
 	fmt.Println(getSumGen(5.4, 3))
+
+	var newCustomer Customer
+	newCustomer.name = "Raheem"
+	newCustomer.city = "Lagos"
+	newCustomer.bal = 6.99
+
+	fmt.Println(newCustomer)
+
+	editCity(&newCustomer, "Ogun")
+	fmt.Println(newCustomer)
 }
