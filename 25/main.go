6a808a7802426a4fc8696e8f8355d43362c599f6
Raheem Kawojue <kawojue08@gmail.com>
2023-12-07 12:57:04 +0100

    working with datetime and random

--- 25/main.go
+++ 25/main.go
@@ -10,11 +10,19 @@ func main() {
 	now := time.Now()
 	date := time.Date(2019, time.December, 21, 05, 42, 0, 0, time.Local)
 
-	fmt.Println(now.Unix())                              // seconds
-	fmt.Println(now.UnixMilli())                         // milliseconds
-	fmt.Println(time.Now().Local().Format("2006-01-02")) // ISOString Date
-	fmt.Println(time.Now().Compare(date.Local()))        // 1 -> future
-	fmt.Println(date.Local().Compare(time.Now()))        // -1 -> expired
+	fmt.Println(now.Unix())                                   // seconds
+	fmt.Println(now.UnixMilli())                              // milliseconds
+	fmt.Println(now.UTC().Format("2006-01-02T15:04:05.999Z")) // ISO String Date
+	fmt.Println(now.Compare(date.Local()))                    // 1 -> future
+	fmt.Println(date.Local().Compare(now))                    // -1 -> expired
+
+	formatDuration, err := time.ParseDuration("1h29m0s")
+	if err != nil {
+		fmt.Println(err)
+	}
+	future := now.Add(formatDuration)
+
+	fmt.Println(future)
 
 	randInt := rand.Intn(2) // 0 <= randInt < 2
 	fmt.Println(randInt)
