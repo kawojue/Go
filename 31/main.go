9d6bbda2323146ff77793f82143cf6da36c80d9f
Raheem Kawojue <kawojue08@gmail.com>
2023-12-11 22:49:08 +0100

    working with closure

--- 31/main.go
+++ 31/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	intSum := func(x int, y int) int { return x + y }
 
-	fmt.Println(intSum(2, 3))
+	fmt.Println(intSum(2, 3)) // 5
+
+	samp1 := 1
+	func() {
+		samp1 += 1
+	}()
+
+	fmt.Println("Samp 1:", samp1) // 2
+	// this is not supposed to happen but cuz it is closure.
 }
