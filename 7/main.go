04c9f6005f2cde553436b185f86013eaafc52a5f
Raheem Kawojue <kawojue08@gmail.com>
2023-12-21 12:38:36 +0100

    for loop

--- 7/main.go
+++ 7/main.go
@@ -2,17 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	x := 0
+var (
+	x      int   = 0
+	scores []int = []int{1, 4, 6, 2, 3}
+)
 
+func main() {
 	for x < 5 {
 		fmt.Printf("Value of x: %d\n", x)
 		x++
 	}
 
 	fmt.Println()
-
-	scores := []int{1, 4, 6, 2, 3}
 
 	for i := 0; i < len(scores); i++ {
 		fmt.Printf("score [%d]: %d\n", i, scores[i])
